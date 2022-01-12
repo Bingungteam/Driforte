d40d799df850cc3c0ed3355fb8db5b97b2985fa2
Mikael Petrus Saragi <mikaelsaragi29@gmail.com>
2022-01-13 02:12:53 +0700

    Perbaikan gitignore

--- Assets/Scripts/car/CarController.cs
+++ Assets/Scripts/car/CarController.cs
@@ -5,21 +5,25 @@ using UnityEngine;
 public class CarController : MonoBehaviour
 {
     public Rigidbody2D car;
+    public SpawnerMobil spawn;
     public Animator anim;
     public float speed;
     public GameObject balonChar;
+    public GameObject EmotFeedback;
+
     // Start is called before the first frame update
     void Start()
     {
         car = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
-        StartCoroutine(HideBaloon());
+        StartCoroutine(HideBaloon(3));
+        spawn = GameObject.Find("Gameplay/SpawnController").GetComponent<SpawnerMobil>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
         if (speed > 0.1f)
         {
 
@@ -30,29 +34,44 @@ public class CarController : MonoBehaviour
             car.velocity = Vector2.zero;
             anim.enabled = false;
         }
-        
+
     }
-   
-   void OnTriggerEnter2D(Collider2D collision)
+
+    void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "ticket")
         {
             speed = 0f;
-           StartCoroutine(NextMovement());
-        
+            StartCoroutine(NextMovement(5));
+
         }
-      
+        if (collision.gameObject.tag == "theatre")
+        {
+            speed = 0f;
+            spawn.CallIE(this.gameObject);
+
+        }
+        if (collision.gameObject.name == "Destroyer")
+        {
+            Destroy(gameObject);
+        }
     }
-    IEnumerator NextMovement()
+    public void MoveCar()
     {
-        yield return new WaitForSeconds(5);
+        StartCoroutine(NextMovement(7));
+    }
+
+
+    IEnumerator NextMovement(float time)
+    {
+        yield return new WaitForSeconds(time);
         speed = 5f;
         anim.enabled = true;
     }
-   IEnumerator HideBaloon()
+    IEnumerator HideBaloon(float time)
     {
-        yield return new WaitForSeconds(3);
+        yield return new WaitForSeconds(time);
         balonChar.SetActive(false);
-      
+
     }
 }
--- Assets/Scripts/car/SpawnerMobil.cs
+++ Assets/Scripts/car/SpawnerMobil.cs
@@ -4,29 +4,43 @@ using UnityEngine;
 
 public class SpawnerMobil : MonoBehaviour
 {
-    
+
     public GameObject[] prefabPool;
     private int randomPrefab;
     public Transform spawnPos;
     private float spawnTime = 10f;
     // Start is called before the first frame update
-    
+
     void Start()
 
     {
-        
+
         Spawn();
         InvokeRepeating("Spawn", spawnTime, spawnTime);
-     
+
+    }
+    IEnumerator SetCarWhenOut(float time, GameObject Mobil)
+    {
+        yield return new WaitForSeconds(time);
+        Mobil.SetActive(false);
+        yield return new WaitForSeconds(5);
+        Mobil.GetComponent<BoxCollider2D>().enabled = false;
+        Mobil.SetActive(true);
+        Mobil.GetComponent<CarController>().EmotFeedback.SetActive(true);
+        Mobil.GetComponent<CarController>().MoveCar();
+        yield return new WaitForSeconds(10);
+        Mobil.GetComponent<BoxCollider2D>().enabled = true;
+
+    }
+    public void CallIE(GameObject Mobil)
+    {
+        StartCoroutine(SetCarWhenOut(2, Mobil));
     }
     void Spawn()
-    { 
-        randomPrefab = Random.Range(0, prefabPool.Length);
-        Instantiate(prefabPool[randomPrefab], spawnPos.position,spawnPos.rotation);
-    }
-    void hide()
     {
-       
+        randomPrefab = Random.Range(0, prefabPool.Length);
+        Instantiate(prefabPool[randomPrefab], spawnPos.position, spawnPos.rotation);
     }
-   
+
+
 }
